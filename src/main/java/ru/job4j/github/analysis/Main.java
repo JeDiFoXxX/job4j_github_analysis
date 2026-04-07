233d117b49743af7da60de276899922bcbb616d0
JeDiFoXxX <serjik1993991@gmail.com>
2026-04-07 15:44:17 +0300

    MySetting

--- src/main/java/ru/job4j/github/analysis/Main.java
+++ src/main/java/ru/job4j/github/analysis/Main.java
@@ -1,7 +1,15 @@
 package ru.job4j.github.analysis;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableAsync
+@EnableScheduling
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        SpringApplication.run(Main.class, args);
     }
 }
