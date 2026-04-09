1b106948378c4712192627caadf41235a4af461c
JeDiFoXxX <serjik1993991@gmail.com>
2026-04-09 22:17:25 +0300

    Исправление ошибки

--- src/main/java/ru/job4j/github/analysis/repository/RepositoryRepository.java
+++ src/main/java/ru/job4j/github/analysis/repository/RepositoryRepository.java
@@ -3,5 +3,5 @@ package ru.job4j.github.analysis.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.job4j.github.analysis.model.Repository;
 
-public interface RepositoryRepository extends JpaRepository<Repository, Long>{ }
+public interface RepositoryRepository extends JpaRepository<Repository, Long> { }
 
